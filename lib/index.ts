b98796b0847f752a00e9ea79b50ff95d63458608
Mahad Munir <mahad@xconn.io>
2025-11-06 19:48:57 +0500

    export classes from wampproto

--- lib/index.ts
+++ lib/index.ts
@@ -4,12 +4,18 @@ export {Session} from './session'
 export {IBaseSession, BaseSession, Result, Registration, Invocation, Subscription, Event} from "./types"
 
 export {
+    type Message,
+
+    type Serializer,
     JSONSerializer,
     CBORSerializer,
     MsgPackSerializer,
 
+    type ClientAuthenticator,
     AnonymousAuthenticator,
     TicketAuthenticator,
     WAMPCRAAuthenticator,
     CryptoSignAuthenticator,
+
+    SessionDetails,
 } from "wampproto";
